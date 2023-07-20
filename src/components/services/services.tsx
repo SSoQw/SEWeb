5b2e5ae81afc2b1a4efaf3890b50d466e131a1db
Reilly LeBlanc <Reillyleblanc@gmail.com>
2023-07-20 15:49:53 -0400

    Updated Services

--- src/components/services/services.tsx
+++ src/components/services/services.tsx
@@ -66,7 +66,6 @@ const ServicesPage: React.FC = () => {
     };
 
     const renderDescriptionWithParagraphs = (description: string) => {
-        //TODO: Change to make have the service cards take in HTML ref from JSON, rather than string from JSON
         const lines = description.split('\n\n');
         return lines.map((line, index) => (
             <p key={index}>{line}</p>
@@ -76,34 +75,97 @@ const ServicesPage: React.FC = () => {
     return (
         <div>
             <div>
-            <h1>Our Services</h1>
-            {services.map((service: Service, index: number) => (
-                <div
-                    key={service.id}
-                    id={`accordion-card-${index}`}
-                    className={`accordion-card ${expandedCard === index ? 'expanded' : ''}`}
-                    onClick={() => toggleExpand(index)}
-                >
-                    <div className="accordion-header">
-                        <span>{service.type}</span>
-                        <span className={`expand-icon ${expandedCard === index ? 'minus' : 'plus'}`}>
-              {expandedCard === index ? '-' : '+'}
-            </span>
-                    </div>
-                    {expandedCard === index && (
-                        <div className="accordion-content">
-                            <img src={service.image} alt={service.type} />
-                            {renderDescriptionWithParagraphs(service.longDescription)}
+                <h1>Our Services</h1>
+                {services.map((service: Service, index: number) => (
+                    <div
+                        key={service.id}
+                        id={`accordion-card-${index}`}
+                        className={`accordion-card ${expandedCard === index ? 'expanded' : ''}`}
+                        onClick={() => toggleExpand(index)}
+                    >
+                        <div className="accordion-header">
+                            <span>{service.type}</span>
+                            <span className={`expand-icon ${expandedCard === index ? 'minus' : 'plus'}`}>
+                {expandedCard === index ? '-' : '+'}
+              </span>
                         </div>
-                    )}
-                </div>
-            ))}
-        </div>
+                        {expandedCard === index && (
+                            <div className="accordion-content">
+                                {service.type === 'Camera Systems' ? (
+                                    <>
+                                        <img src={service.image} alt={service.type} />
+                                        {renderDescriptionWithParagraphs(service.longDescription)}
+                                        <CameraSystemsTable />
+                                    </>
+                                ) : (
+                                    <>
+                                        <img src={service.image} alt={service.type} />
+                                        {renderDescriptionWithParagraphs(service.longDescription)}
+                                    </>
+                                )}
+                            </div>
+                        )}
+                    </div>
+                ))}
+            </div>
             <h2>Read About Our Work</h2>
             <Link to="/testimonials" className="big-button">
                 Client Testimonials
             </Link>
         </div>
+    );
+};
+
+const CameraSystemsTable = () => {
+    return (
+        <table className="camera-systems-table">
+            <tbody>
+            <tr>
+                <th className="camera-systems-table-header">Camera System</th>
+                <td className="camera-systems-table-name">Ubiquiti</td>
+                <td className="camera-systems-table-name">Eufy</td>
+            </tr>
+            <tr>
+                <th className="camera-systems-table-header">Resolution</th>
+                <td className="camera-systems-table-cell" colSpan={2}>1080p - 4K</td>
+            </tr>
+            <tr>
+                <th className="camera-systems-table-header">Base Station Cost</th>
+                <td className="camera-systems-table-cell">$379</td>
+                <td className="camera-systems-table-cell">$150</td>
+            </tr>
+            <tr>
+                <th className="camera-systems-table-header">Cost per Camera</th>
+                <td className="camera-systems-table-cell">$100 - $200</td>
+                <td className="camera-systems-table-cell">$50 - $100</td>
+            </tr>
+            <tr>
+                <th className="camera-systems-table-header">Indoor Use</th>
+                <td className="camera-systems-table-cell good">✓</td>
+                <td className="camera-systems-table-cell good">✓</td>
+            </tr>
+            <tr>
+                <th className="camera-systems-table-header">Outdoor Use</th>
+                <td className="camera-systems-table-cell good">✓</td>
+                <td className="camera-systems-table-cell good">✓</td>
+            </tr>
+            <tr>
+                <th className="camera-systems-table-header">Wireless</th>
+                <td className="camera-systems-table-cell partial">~</td>
+                <td className="camera-systems-table-cell good">✓</td>
+            </tr>
+            <tr>
+                <th className="camera-systems-table-header">Hardwired</th>
+                <td className="camera-systems-table-cell good">✓</td>
+                <td className="camera-systems-table-cell bad">x</td>
+            </tr>
+            <tr>
+                <th className="camera-systems-table-header">PTZ</th>
+                <td className="camera-systems-table-cell partial">~</td>
+                <td className="camera-systems-table-cell partial">~</td>
+            </tr>
+            </tbody>
+        </table>
     );
 };
 
